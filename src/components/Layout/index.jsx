0a07795e156d1ce3e3f55f6c28a5329c029c7164
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-04-25 14:23:57 +0800

    Building the Navigation

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -3,6 +3,10 @@ import LOGO from '../../assets/img/logo.png';
 import MagnifyingGlass from '../../Icons/MagnifyingGlass';
 import Bookmark from '../../Icons/Bookmark';
 import Chat from '../../Icons/Chat';
+import Home from '../../Icons/Home';
+import Airplane from '../../Icons/Airplane';
+import Key from '../../Icons/Key';
+import Map from '../../Icons/Map';
 import USER from '../../assets/img/user.png';
 
 const Layout = () => {
@@ -47,7 +51,47 @@ const Layout = () => {
       </header>
 
       <div className="content">
-        <nav className="sidebar">NAVIGATION</nav>
+        <nav className="sidebar">
+          <ul className="side-nav">
+            <li className="side-nav__item side-nav__item--active">
+              <a href="#" className="side-nav__link">
+                <span className="side-nav__icon">
+                  <Home />
+                </span>
+                <span>Hotel</span>
+              </a>
+            </li>
+            <li className="side-nav__item">
+              <a href="#" className="side-nav__link">
+                <span className="side-nav__icon">
+                  <Airplane />
+                </span>
+                <span>Flight</span>
+              </a>
+            </li>
+            <li className="side-nav__item">
+              <a href="#" className="side-nav__link">
+                <span className="side-nav__icon">
+                  <Key />
+                </span>
+                <span>Car Rental</span>
+              </a>
+            </li>
+            <li className="side-nav__item">
+              <a href="#" className="side-nav__link">
+                <span className="side-nav__icon">
+                  <Map />
+                </span>
+                <span>Tours</span>
+              </a>
+            </li>
+          </ul>
+
+          <div className="legal">
+            &copy; 2020 by trillo. All rights reserved.
+          </div>
+        </nav>
+
         <main className="hotel-view">HOTEL VIEW</main>
       </div>
     </div>
