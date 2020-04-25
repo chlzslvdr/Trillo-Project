f51506296333c37d78893fab7ff333c4dc8338c6
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-04-25 20:42:42 +0800

    Building the Description Section

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -10,6 +10,10 @@ import Map from '../../Icons/Map';
 import Star from '../../Icons/Star';
 import Location from '../../Icons/Location';
 import USER from '../../assets/img/user.png';
+import USER3 from '../../assets/img/user-3.png';
+import USER4 from '../../assets/img/user-4.png';
+import USER5 from '../../assets/img/user-5.png';
+import USER6 from '../../assets/img/user-6.png';
 
 import HOTEL1 from '../../assets/img/hotel-1.jpg';
 import HOTEL2 from '../../assets/img/hotel-2.jpg';
@@ -143,6 +147,61 @@ const Layout = () => {
               <div className="overview__rating-average">9.2</div>
               <div className="overview__rating-count">720 votes</div>
             </div>
+          </div>
+
+          <div className="detail">
+            <div className="description">
+              <p className="paragraph">
+                Lobortis porta magna nullam odio tempor bibendum penatibus
+                sociis dui, torquent condimentum fermentum felis habitant
+                facilisis curabitur interdum ullamcorper ultricies convallis.
+              </p>
+              <p className="paragraph">
+                Nulla habitasse turpis diam lacinia tincidunt, potenti sodales
+                gravida pharetra vivamus,mollis pharetra nullam ligula.
+              </p>
+
+              <ul className="list">
+                <li className="list__item">Close to the beach</li>
+                <li className="list__item">Breakfast included</li>
+                <li className="list__item">Free airport shuttle</li>
+                <li className="list__item">Free wifi in all rooms</li>
+                <li className="list__item">Air conditioning and heating</li>
+                <li className="list__item">Pets allowed</li>
+                <li className="list__item">We speak all languages</li>
+                <li className="list__item">Perfect for families</li>
+              </ul>
+
+              <div className="recommend">
+                <p className="recommend__count">
+                  Seo Joon and 3 other friends recommend this hotel.
+                </p>
+                <div className="recommend__friends">
+                  <img
+                    src={USER6}
+                    alt="Friend 1"
+                    className="recommend__photo"
+                  />
+                  <img
+                    src={USER4}
+                    alt="Friend 2"
+                    className="recommend__photo"
+                  />
+                  <img
+                    src={USER5}
+                    alt="Friend 3"
+                    className="recommend__photo"
+                  />
+                  <img
+                    src={USER3}
+                    alt="Friend 4"
+                    className="recommend__photo"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="user-reviews">user reviews </div>
           </div>
         </main>
       </div>
