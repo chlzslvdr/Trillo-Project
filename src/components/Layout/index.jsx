20af7c0f0032b73f371457ce1103bf9fce2a6f9c
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-04-25 09:14:59 +0800

    add svg icons

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import LOGO from '../../assets/img/logo.png';
 
 const Layout = () => {
   return (
     <div className="container">
-      <header className="header">HEADER</header>
+      <header className="header">
+        <img src={LOGO} alt="Trillo" className="logo" />
+
+        <form action="#" className="search">
+          <input
+            type="text"
+            className="search__input"
+            placeholder="Seach hotels"
+          />
+          <button className="search__button">
+            <svg className="search__icon">
+              <use xlinkHref="src/assets/img/sprite.svg#magnifying-glass" />
+            </svg>
+          </button>
+        </form>
+      </header>
 
       <div className="content">
         <nav className="sidebar">NAVIGATION</nav>
